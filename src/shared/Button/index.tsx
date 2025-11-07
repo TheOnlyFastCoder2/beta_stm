829e9b81da970e023e1f4056b68ed92db423c09a
TheOnlyFastCoder2 <ponomareow.alex2018@gmail.com>
2025-11-08 00:12:41 +0400

    JUST SAVE

--- src/shared/Button/index.tsx
+++ src/shared/Button/index.tsx
@@ -8,35 +8,35 @@ export default function Button({
   onClick,
   ...props
 }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
-  // const store = useSignalStore({
-  //   x: 0,
-  //   y: 0,
-  // });
-  // const timeId = useRef<number | undefined>(undefined);
+  const store = useSignalStore({
+    x: 0,
+    y: 0,
+  });
+  const timeId = useRef<number | undefined>(undefined);
 
-  // const ref = store.useComputed<HTMLDivElement>(({ current: el }) => {
-  //   el.style.left = `${store.$.x.v}px`;
-  //   el.style.top = `${store.$.y.v}px`;
-  //   clearTimeout(timeId.current);
-  //   el.classList.remove($.animation);
-  //   void el.offsetWidth;
-  //   el.classList.add($.animation);
-  //   timeId.current = setTimeout(() => {
-  //     el.classList.remove($.animation);
-  //   }, 600);
-  // });
+  const ref = store.useComputed<HTMLDivElement>(({ current: el }) => {
+    el.style.left = `${store.$.x.v}px`;
+    el.style.top = `${store.$.y.v}px`;
+    clearTimeout(timeId.current);
+    el.classList.remove($.animation);
+    void el.offsetWidth;
+    el.classList.add($.animation);
+    timeId.current = setTimeout(() => {
+      el.classList.remove($.animation);
+    }, 600);
+  });
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    // const rect = e.currentTarget.getBoundingClientRect();
-    // store.$.x.v = e.clientX - rect.left;
-    // store.$.y.v = e.clientY - rect.top;
+    const rect = e.currentTarget.getBoundingClientRect();
+    store.$.x.v = e.clientX - rect.left;
+    store.$.y.v = e.clientY - rect.top;
     onClick?.(e);
   };
 
   return (
     <button {...props} className={`${$.Button} ${className}`} onClick={handleClick}>
       <div className={ $.Button_wrapper } /> 
-      {/* ref={ref} */}
+      ref={ref}
       {children}
     </button>
   );
